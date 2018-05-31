088eb6746191e1d1295c33879db26790ae604f3a
Ayoub Oudmane <ayoub@oudmane.me>
2018-05-31 23:41:42 +0000

    + component.beforeDestroy

--- source/component.js
+++ source/component.js
@@ -1,19 +1,27 @@
 export default {
     extend(component) {
 
-        let render = component.render
+        let render = component.render,
+            beforeDestroy = component.beforeDestroy
 
-        component.render = function(h) {
+        if(render)
+            component.render = function(h) {
 
-            return render.bind(this)(
-                h,
-                {
-                    // state: this.$store.state,
-                    data: this.$data,
-                    $t: this.$t.bind(this)
-                }
-            )
+                return render.bind(this)(
+                    h,
+                    {
+                        state: this.$store.state,
+                        data: this.$data,
+                        $t: this.$t.bind(this)
+                    }
+                )
 
+            }
+
+        component.beforeDestroy = function() {
+            this.$emit('beforeDestroy')
+            if(beforeDestroy)
+                beforeDestroy.bind(this)()
         }
 
         return component
